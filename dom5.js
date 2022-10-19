67affa1585c5100b2b9fa182a5aa3a6293ad8612
Sushmitacode12 <sushmitatripathi786@gmail.com>
2022-10-19 11:08:19 +0530

    my task5

---
+++ dom5.js
@@ -0,0 +1,6 @@
+var li = document.getElementsByTagName("li");
+li[4].textContent = 'hii';
+li[4].style.backgroundColor = 'yellow';
+
+var items = document.getElementsByClassName('list-group-item');
+items[0].style.fontWeight = 'bold';
